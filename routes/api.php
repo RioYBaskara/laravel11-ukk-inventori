60f5bd7b6a84a6c807060aad13eebab361f872b0
Rio Yudha Baskara <156874101+RioYBaskara@users.noreply.github.com>
2024-06-11 16:22:55 +0700

    cleaning project dari testing

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ApiKategoriController;
-use App\Http\Controllers\ApiKatControllerResource;
+use App\Http\Controllers\Api\ApiKatControllerResource;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('kategori', ApiKategoriController::class);
-
-Route::get('apiKategori', [ApiKategoriController::class, 'getAPIKategori']);
-
-Route::post('kategori/{kategori_id}', [ApiKategoriController::class, 'updateKategori']);
-
-Route::apiResource('kategorires', ApiKatControllerResource::class);
+Route::apiResource('kategori', ApiKatControllerResource::class);
--- routes/web.php
+++ routes/web.php
@@ -4,22 +4,13 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\SiswaController;
 use App\Http\Controllers\LoginController;
-use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\BarangController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\BarangMasukController;
 use App\Http\Controllers\BarangKeluarController;
-use App\Http\Controllers\TesController;
 
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::resource('/products', \App\Http\Controllers\ProductController::class);
-
-Route::resource('/barang', BarangController::class)->middleware('auth');
+Route::resource('/', BarangController::class)->middleware('auth');
 
 Route::get('login', [LoginController::class,'index'])->name('login')->middleware('guest');
 Route::post('login', [LoginController::class,'authenticate']);
@@ -33,4 +24,8 @@ Route::resource('kategori', KategoriController::class)->middleware('auth');
 Route::resource('barangmasuk',BarangMasukController::class)->middleware('auth');
 Route::resource('barangkeluar',BarangKeluarController::class)->middleware('auth');
 
-Route::resource('tes', TesController::class)->middleware('auth');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::resource('tes', TesController::class)->middleware('auth');
