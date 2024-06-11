cd5d01c1a25e69f9307628387bad97d999bc0e30
Rio Yudha Baskara <156874101+RioYBaskara@users.noreply.github.com>
2024-06-12 00:11:22 +0700

    title

--- resources/views/layouts/adm-main.blade.php
+++ resources/views/layouts/adm-main.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>GuruKoding</title>
+    <title>Inventori Rio</title>
 
     <!-- Custom fonts for this template-->
     <link href="{!! asset('theme/vendor/fontawesome-free/css/all.min.css') !!}" rel="stylesheet" type="text/css">
